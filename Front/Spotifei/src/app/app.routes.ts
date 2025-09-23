58e6238375aaa059358ebcb72d64cdc8554784a6
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-09-23 11:09:39 -0300

    criação da tela de login

--- Front/Spotifei/src/app/app.routes.ts
+++ Front/Spotifei/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { EquipeComponent } from './equipe/equipe.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './layout/login/login.component';
 
 export const routes: Routes = [
     { path: 'equipe', component: EquipeComponent },
+    { path: 'layout', component: LayoutComponent},
+    { path: 'login', component: LoginComponent},
 ];
