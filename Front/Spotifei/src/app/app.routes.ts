3e7ffd201c512399ca45584002b4190d5188c5ff
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-09-23 20:23:41 -0300

    att layout funcional

--- Front/Spotifei/src/app/app.routes.ts
+++ Front/Spotifei/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { EquipeComponent } from './equipe/equipe.component';
 import { PerfilComponent } from './layout/perfil/perfil.component';
 import { LoginComponent } from './layout/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     { path: "equipe", component: EquipeComponent },
     {path: "perfil", component: PerfilComponent},
     {path: "login", component: LoginComponent},
+    {path: "layout", component: LayoutComponent},
     {
         path: "",
         redirectTo: "login",
